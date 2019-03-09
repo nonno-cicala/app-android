98a02eed1de974b2e211a422a976bffc70b25307
Marcoski <mtrognon@gmail.com>
2019-03-09 16:01:33 +0100

    Soluzione: Scrivere un algoritmo che, date le età di tre persone, calcoli l’età media.

---
+++ esercizi/etamedia.js
@@ -0,0 +1,8 @@
+/* SOLUZIONE DELL'ESERCIZIO: Scrivere un algoritmo che, date le età di tre persone, calcoli l’età media. */
+
+const age1 = Number(prompt('Inserisci l\'età della prima persona'))
+const age2 = Number(prompt('Inserisci l\'età della seconda persona'))
+const age3 = Number(prompt('Inderisci l\'età della terza persona'))
+
+const avg = (age1 + age2 + age3) / 3
+console.log(`La media è di ${avg} anni`)
