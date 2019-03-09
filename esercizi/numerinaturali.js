7bcc4cf5f8a1cb1865ca54ccbb2078d322df961a
Marcoski <mtrognon@gmail.com>
2019-03-09 16:26:07 +0100

    Create numerinaturali.js

---
+++ esercizi/numerinaturali.js
@@ -0,0 +1,18 @@
+/** 
+* SOLUZIONE DELL'ESERCIZIO: 
+* Scrivere un algoritmo che visualizza i numeri naturali da 1 a 20
+* CODEPEN: https://codepen.io/marcoski/pen/jJwaqb
+*/
+// VERSIONE WHILE DO
+console.log('VERSIONE WHILE')
+let x = 1
+while (x <= 20) {
+  console.log(x)
+  x += 1
+}
+
+// VERSIONE FOR
+console.log('VERSIONE FOR')
+for (let x = 0; x <= 20; x++) {
+  console.log(x)
+}
